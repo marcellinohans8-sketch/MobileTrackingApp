cfe1b6ce1e7cc7bc5d405a5f769965813a5354cc
marcellinohans8-sketch <marcellinohans8@gmail.com>
2026-06-20 14:15:18 +0700

    feat: setup navigation

---
+++ src/app/navigation/AppNavigator.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from '../../features/auth/screens/LoginScreen';
+import TrackingScreen from '../../features/tracking/screens/TrackingScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+
+        <Stack.Screen name="Tracking" component={TrackingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
